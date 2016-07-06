dc3ff5c18b06c1273d436c0dd6ba8b59adaa3614
hect1995 <hect.esteban@gmail.com>
2016-07-07 00:31:48 +0200

    Update regression.py

--- regression.py
+++ regression.py
@@ -4,7 +4,7 @@ num_puntos = 100
 conjunto_puntos = []
 for i in xrange(num_puntos):
 	x1= np.random.normal(0.0, 0.9)
-	y1= x1 * 0.1 + 0.3 + np.random.normal(0.0, 0.05)
+	y1= x1 * 0.3 + 0.9 + np.random.normal(0.0, 0.05)
 	conjunto_puntos.append([x1, y1])
 
 x_data = [v[0] for v in conjunto_puntos]
